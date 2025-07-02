edb0df7fe7011b1adb2a9ab2980936869d07cbdb
fabrikant-bscott <brandon@fabrikant.tech>
2025-07-02 14:46:49 -0400

    feat: compare two remote versions instead of against local

--- src/commands/diff.ts
+++ src/commands/diff.ts
@@ -5,22 +5,19 @@ import { getCredentialsOrThrow } from '../utils/auth-utils.js';
 import { constructive, destructive, primary } from '../utils/colorize.js';
 import { isNotEmpty } from '../utils/collection-utils.js';
 import { select } from '@inquirer/prompts';
-import {
-  cleanSemver,
-  getVersionDisplayName,
-  isSemver,
-  isVersionId,
-} from '../utils/version-utils.js';
-import { compact, isEmpty } from 'lodash-es';
+import { findVersionByIdOrSemver, getVersionDisplayName } from '../utils/version-utils.js';
+import { compact, isEmpty, isString } from 'lodash-es';
 import type { VersionDto } from '../types/index.js';
 import { globby } from 'globby';
 import path from 'node:path';
 import { cwd } from 'node:process';
 import { readFile } from 'node:fs/promises';
-import { normalizeSourceFiles } from '../utils/source-file-utils.js';
+import { filterNormalizedSourceFiles, normalizeSourceFiles } from '../utils/source-file-utils.js';
 import type { StructuredPatch } from 'diff';
 import { structuredPatch } from 'diff';
 import { pager } from '../utils/pager.js';
+import { Errors } from '@oclif/core';
+import type { CommandError } from '@oclif/core/interfaces';
 
 class Diff extends BaseCommand {
   static args = {
@@ -38,15 +35,31 @@ class Diff extends BaseCommand {
   static description = 'Diff file(s) between two versions';
   static examples = [];
   static flags = {
+    from: Flags.string({
+      char: 'f',
+      description:
+        'Id or published version number to diff files from. For example: v0.0.42, 0.0.42, or 66461c33e633cbb0adf030ab',
+      exclusive: ['directory', 'version'],
+      relationships: [{ type: 'all', flags: ['to'] }],
+    }),
+    to: Flags.string({
+      char: 't',
+      description:
+        'Id or published version number to diff files against. For example: v0.0.43, 0.0.43, or 66461c33e633cbb0adf030ab',
+      exclusive: ['directory', 'version'],
+      relationships: [{ type: 'all', flags: ['from'] }],
+    }),
     directory: Flags.string({
       char: 'd',
       description: 'Directory to read source files from.',
       default: '.',
+      exclusive: ['from', 'to'],
     }),
     version: Flags.string({
       char: 'v',
       description:
         'Id or published version number to diff files. For example: v0.0.43, 0.0.43, or 66461c33e633cbb0adf030ab',
+      exclusive: ['from', 'to'],
     }),
   };
 
@@ -56,36 +69,62 @@ class Diff extends BaseCommand {
     const directory = path.resolve(cwd(), flags.directory);
     const { token } = await getCredentialsOrThrow();
     const versions = await listVersions({ token });
-
+    let versionIdOrSemver: string | undefined = flags.version;
     let version: VersionDto | undefined = undefined;
-    let semanticVersion =
-      isNotEmpty(flags.version) && isSemver(flags.version) ? cleanSemver(flags.version) : undefined;
-    let versionId =
-      isNotEmpty(flags.version) && isVersionId(flags.version) ? flags.version : undefined;
 
-    if (isEmpty(semanticVersion) && isEmpty(versionId)) {
+    if (isNotEmpty(flags.to) && isNotEmpty(flags.from)) {
+      const fromVersion = findVersionByIdOrSemver(versions, flags.from);
+      const toVersion = findVersionByIdOrSemver(versions, flags.to);
+      if (fromVersion === undefined) {
+        this.error(destructive(`Version '${flags.from}' not found.`));
+      }
+
+      if (toVersion === undefined) {
+        this.error(destructive(`Version '${flags.to}' not found.`));
+      }
+
+      ux.action.start(
+        `Retrieving files for ${getVersionDisplayName(fromVersion)} and ${getVersionDisplayName(toVersion)}`
+      );
+      const [_sourceSourceFiles, _targetSourceFiles] = await Promise.all([
+        getNormalizedSourceFilesByVersion({
+          token,
+          versionId: fromVersion._id,
+        }),
+        getNormalizedSourceFilesByVersion({
+          token,
+          versionId: toVersion._id,
+        }),
+      ]);
+      ux.action.stop(constructive('✔'));
+      const sourceSourceFiles = filterNormalizedSourceFiles(_sourceSourceFiles, paths);
+      const targetSourceFiles = filterNormalizedSourceFiles(_targetSourceFiles, paths);
+
+      const patches = buildDiffPatches(sourceSourceFiles, targetSourceFiles);
+      const formattedPatches = patches.map(formatPatch);
+
+      await pager(formattedPatches.join('\n'));
+
+      this.exit();
+    }
+
+    if (isEmpty(versionIdOrSemver)) {
       const choices = versions.map((version) => ({
         name: getVersionDisplayName(version),
         description: `Id: ${version._id}${isNotEmpty(version.published_at) ? ` | Published on ${new Date(version.published_at).toLocaleString()}` : ' | Unpublished'}`,
         value: version._id,
       }));
 
-      versionId = await select({
+      versionIdOrSemver = await select({
         message: 'Select a version to diff design system files from',
         choices,
       });
     }
 
-    if (isNotEmpty(semanticVersion)) {
-      version = versions.find((version) => version.publish_version === semanticVersion);
-    }
-
-    if (isNotEmpty(versionId)) {
-      version = versions.find((version) => version._id === versionId);
-    }
+    version = findVersionByIdOrSemver(versions, versionIdOrSemver);
 
     if (version === undefined) {
-      this.error(destructive(`Version '${semanticVersion ?? versionId}' not found.`));
+      this.error(destructive(`Version '${versionIdOrSemver}' not found.`));
     }
 
     const filePaths = await globby([...paths, '!node_modules', '!dist', '!build'], {
